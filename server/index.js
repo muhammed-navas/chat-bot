52e85eb9b9f63f267891c0de8749e92fe2e8fc78
muhammed navas <muhammednavas6287@gmail.com>
2025-01-21 23:48:47 +0530

    some change

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
-import pool from './config/db.js';
 import router from "./routes/route.js";
 
 dotenv.config();
